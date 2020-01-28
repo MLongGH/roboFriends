8731173cf9885d022ba44fd463e08d613bc16d07
meilinglong <meilingl.usu@gmail.com>
2020-01-28 15:20:43 -0700

    add Header component to improve performance

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,8 @@ import SearchBox from '../components/SearchBox';
 import CardList from '../components/CardList';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
+import Header from '../components/Header';
+
 import './App.css';
 
 import {setSearchField, requestRobots} from "../action";
@@ -56,7 +58,7 @@ class App extends Component {
     } else {
       return (
           <div className='tc'>
-            <h1 className='f1'>RoboFriends</h1>
+            <Header/>
             <SearchBox searchChange={onSearchChange}/>
             <Scroll>
               <ErrorBoundry>
