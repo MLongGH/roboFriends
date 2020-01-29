71d2779efc393d5c02f8eb4b44ccbd584f8a7073
meilinglong <meilingl.usu@gmail.com>
2020-01-29 10:59:08 -0700

    changes for progressive web app

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
   const {name, email, id} = props;
   return (
       <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-        <img alt='robot' src={`https://robohash.org/${id}?100*100`}/>
+        <img alt='robot' src={`https://robohash.org/${id}?200*200`}/>
         <div>
           <h2>{name}</h2>
           <p>{email}</p>
