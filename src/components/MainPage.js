be9fb8a3907b70a6bb8213da787db2fa90668df0
meilinglong <meilingl.usu@gmail.com>
2020-01-30 20:07:38 -0700

    test connected component

---
+++ src/components/MainPage.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import SearchBox from '../components/SearchBox';
+import CardList from '../components/CardList';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+import Header from '../components/Header';
+
+import './MainPage.css';
+
+class MainPage extends Component {
+
+  componentDidMount() {
+    this.props.onRequestRobots();
+  }
+
+  filterRobots = () => {
+    return this.props.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.props.searchField.toLowerCase());
+    });
+  }
+
+  render() {
+    const {onSearchChange, isPending} = this.props;
+
+    if (isPending) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+          <div className='tc'>
+            <Header/>
+            <SearchBox searchChange={onSearchChange}/>
+            <Scroll>
+              <ErrorBoundry>
+                <CardList robots={this.filterRobots()}/>
+              </ErrorBoundry>
+            </Scroll>
+          </div>
+      );
+    }
+  }
+}
+
+export default MainPage;
